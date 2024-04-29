ef25100826cb1ded18ed5a90de0af87d4742b23e
wangyunzi <xueq695@gmail.com>
2024-04-29 23:19:21 +0800

    修改图片问题

--- themes/zozo/static/js/zozo.js
+++ themes/zozo/static/js/zozo.js
@@ -38,3 +38,16 @@ $(document).ready((function (_this) {
 })(this))
 
 
+// 文件位置：~/blog/node_modules/marked/lib/marked.js图片说明文字
+
+Renderer.prototype.image = function(href, title, text) {
+  if (this.options.baseUrl && !originIndependentUrl.test(href)) {
+    href = resolveUrl(this.options.baseUrl, href);
+  }
+  var out = '<img src="' + href + '" alt="' + text + '"';
+  if (title) {
+    out += '>' + '<i class="img-caption">' + '◎ ' + title + '</i';
+  }
+  out += this.options.xhtml ? '/>' : '>';
+  return out;
+};
